f7ed0650678492e999e1739814a5aae6139b180a
Spaceferkel <141821436+Spaceferkel@users.noreply.github.com>
2023-08-27 15:52:44 +0200

    Mutation.py

--- Mutation.py
+++ Mutation.py
@@ -1,37 +1,24 @@
-
 import random
 
     
 def generate_DNA_seq(DNA_length):
     nucs = ["A", "T", "C", "G"]
     seq = ""
-    
     for i in range(0, DNA_length):
         seq += random.choice(nucs)
-
+        
     return seq
 
 
 
 def mutate(seq):
     #mutates one base in the DNA sequence
-    nucs = ["A", "T", "C", "G"]
     
+    nucs = ["A", "T", "C", "G"]
     position = random.randint(0, len(seq)-1)
     old_nuc = seq[position]
     nucs.remove(old_nuc)
     new_nuc = random.choice(nucs)
-    
     seq_mutated = seq[:position] + new_nuc + seq[position+1:]
+    
     return seq_mutated
-        
-
-
-def count_diff_seq(DNA_dic):
-    count_per_gen = {}
-    for i in DNA_dic:
-        diff_seq = len(set(DNA_dic[i]))
-        count_per_gen[i] = diff_seq
-        
-    return count_per_gen
- 
